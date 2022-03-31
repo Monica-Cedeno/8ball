cf4acd0f05184c5fffc0177cdf750a84aa7bc1d9
Monica  Cedeno <monicacedeno@Monicas-MacBook-Air.local>
2022-03-30 21:55:21 -0400

    created functioning methods. Created working regEx functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
-let answers = document.getElementById("answers")
-let pictures = `<img src = images>`
 
 
+createImage('img/magic8ball_start.png');
 
-function ask(){
-    let pic = Math.floor(Math.random()* pictures.length);
-    let img = `<img src = images/${pic}`;
-    console.log (img);
-    answers.innerHTML = img;
+function ask() {
+    const random = Math.floor(Math.random() * 20)+1
+    const imgSrc = `./img/magic8ball_${random}.png`;
+    createImage(imgSrc)
+} 
+
+function createImage(imgSrc) {
+    const answers = document.getElementById("answers")
+    const response = document.createElement('img');
+    response.height = '200';
+    response.width = '200';
+    response.src = imgSrc;
+
+    answers.innerHTML = " ";
+    answers.appendChild(response);
 }
+
+
+button.addEventListener("click", function() {
+    const questions = document.getElementById("yourQuestions");
+    const expression = new RegExp(/\?$/g);
+
+    if (expression.test(questions.value)){
+        ask()
+    }
+    else if (questions.value < 2){
+        alert ("please type your question")
+    }
+    else {
+        alert ("must include a question mark")
+    }
+});
+
+
+
+
 
 
